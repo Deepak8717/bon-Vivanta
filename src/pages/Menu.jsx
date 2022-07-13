35fbc31953f02d24dfc237df666a7bd33d370603
Deepak8717 <sendit2deepak@gmail.com>
2022-07-13 11:06:07 +0530

    seprated data fetch function

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import Navbar from "../layout/Navbar";
 import Sidebar from "../layout/Sidebar";
 import RecipeGrid from "../components/recipes/RecipeGrid";
 import CustomerInputWidget from "../components/sidebar/CustomerInputWidget";
-const app_Id = process.env.REACT_APP_API_ID;
-const app_Key = process.env.REACT_APP_API_KEY;
+import { fetchMealType } from "../services/fetch-mealtype";
 
 export default function Menu() {
   const [recipes, setRecipes] = useState([]);
@@ -19,19 +17,16 @@ export default function Menu() {
     setActiveMenu(menu);
   };
   // to fetch data
-  // useEffect(() => {
-  //   setLoading(true);
-  //   const baseURL = `https://api.edamam.com/api/recipes/v2?type=public&q=%22%22&app_id=${app_Id}&app_key=${app_Key}&mealType=${activeMenuWithoutChar}&random=true`;
-  //   axios
-  //     .get(baseURL)
-  //     .then((response) => {
-  //       setRecipes(response.data.hits);
-  //       setLoading(false);
-  //     })
-  //     .catch(console.error);
-  // }, [activeMenu]);
-
-  //this is to show the horizontal nav on smaller screen
+  useEffect(() => {
+    setLoading(true);
+    fetchMealType(activeMenuWithoutChar)
+      .then((data) => {
+        setRecipes(data);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [activeMenu]);
+  // this is to show the horizontal nav on smaller screen
   const updateMedia = () => {
     window.innerWidth < 1024 ? setDesktop(false) : setDesktop(true);
   };
